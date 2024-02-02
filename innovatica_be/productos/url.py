a402f6de4db2aa2b666e94607fc303b7d942189d
nanrelo <nanrelo@gmail.com>
2024-02-02 02:41:04 -0300

    * API/VIEW de productos terminada parcialmente.

--- innovatica_be/productos/url.py
+++ innovatica_be/productos/url.py
@@ -2,7 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', get_unsigned_products, name="get_unsigned_products"),
-    path('all/', get_signed_products, name="get_signed_products"),
-    path('insert_product/', insert_product, name="insert_product"),
+    path('', get_unsigned_products, name='get_unsigned_products'),
+    path('all/', get_signed_products, name='get_signed_products'),
+    path('get_product_by_filter/', get_product_by_filter, name='get_product_by_filter'),
+    path('', get_unsigned_products, name='get_unsigned_products'),
+    path('insert_product/', insert_product, name='insert_product'),
+    path('delete_product/', delete_product, name='delete_product'),
+    path('update_product/', update_product, name='update_product'),
 ]
--- innovatica_be/productos/views.py
+++ innovatica_be/productos/views.py
@@ -2,6 +2,7 @@ from django.core import serializers
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+
 from .models import Producto
 
 import json
@@ -10,7 +11,7 @@ import json
 @require_http_methods('GET')
 def get_unsigned_products(request):
     data = serializers.serialize('json', list(Producto.objects.all()))
-    return JsonResponse(data, safe=False)
+    return JsonResponse(json.loads(data), safe=False)
 
 
 @require_http_methods('GET')
@@ -19,22 +20,53 @@ def get_signed_products(request):
 
 
 @require_http_methods('GET')
-def get_unsigned_products(request):
-    data = serializers.serialize('json', list(Producto.objects.filter()))
-    return JsonResponse(data, safe=False)
-
+def get_product_by_filter(request):
+    fiter_data = json.dumps(request.GET)
+    product_query_set = Producto.objects.filter(**json.loads(fiter_data))
+    data = serializers.serialize('json', list(product_query_set))
+    return JsonResponse(json.loads(data), safe=False)
 
 
 @csrf_exempt
 @require_http_methods('POST')
 def insert_product(request):
     new_product = json.loads(request.body)
-    nombre = new_product['nombre']
-    categoria = new_product['categoria']
-    estado = new_product['estado']
-    imagen = new_product['imagen']
-
-    product = Producto(nombre=nombre, categoria=categoria, estado=estado, imagen=imagen)
+    product = Producto(**new_product)
     product.save()
 
-    return JsonResponse({'message': 'Product ' + nombre + ' inserted correctly'})
+    return JsonResponse({'message': 'Product ' + new_product['nombre'] + ' inserted correctly'})
+
+
+@csrf_exempt
+@require_http_methods('DELETE')
+def delete_product(request):
+    data = request.GET
+    product_id = data['id']
+    product_to_delete = Producto.objects.filter(id=product_id)
+    try:
+        if not product_to_delete.exists():
+            raise Exception('Empty QuerySet')
+        product_to_delete.delete()
+
+        return JsonResponse({'message': 'Se ha eliminado el producto ' + product_id + ' con exito!'})
+    except Exception as e:
+        return JsonResponse({'message': 'No se ha podido eliminar el producto ' + product_id, 'error': str(e)},
+                            status=418)
+
+
+@csrf_exempt
+@require_http_methods('PUT')
+def update_product(request):
+    data = request.GET
+    id = data['id']
+    product_new_values = json.loads(request.body)
+    product_to_update = Producto.objects.filter(id=id)
+    try:
+        if not product_to_update.exists():
+            raise Exception('Empty QuerySet')
+        product_to_update.update(**product_new_values)
+
+        return JsonResponse({'message': 'Se ha eliminado el producto ' + id + ' con exito!'})
+    except Exception as e:
+        return JsonResponse({'message': 'No se ha podido eliminar el producto ' + id, 'error': str(e)},
+                            status=418)
