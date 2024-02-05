bd6b769431fa1c2694b8304a95b8a1ea59a2e72c
nanrelo <nanrelo@gmail.com>
2024-02-05 01:37:49 -0300

    * customizando el login

--- innovatica_be/authentification/views.py
+++ innovatica_be/authentification/views.py
@@ -1,11 +1,30 @@
-from django.core import serializers
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+
+
+@api_view(['POST'])
+def custom_login(request):
+    data = request.data
+    serializer = TokenObtainPairSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    user = list(User.objects.filter(username=data['username']).values())[0]
+    token_pair = serializer.validated_data
+
+    custom_data = {
+        'refresh': str(token_pair['refresh']),
+        'access': str(token_pair['access']),
+        'user_id': user['id'],
+        'username': user['username'],
+        'is_superuser': user['is_superuser']
+    }
+
+    return JsonResponse(custom_data)
 
 
 @api_view(['POST'])
